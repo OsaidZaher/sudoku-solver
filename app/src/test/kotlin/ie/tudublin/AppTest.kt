4960388a950e6453cb801312a187dc6c7bd243e3
Osaid Zaher <zaher.osaid@gmail.com>
2025-12-01 17:01:25 +0000

    Add GitHub Actions CI workflow

--- app/src/test/kotlin/ie/tudublin/AppTest.kt
+++ app/src/test/kotlin/ie/tudublin/AppTest.kt
@@ -9,6 +9,7 @@ class SudokuTest {
     fun testSimplePuzzle() {
         println("Testing simple sudoku puzzle...")
         
+        // Simple sudoku puzzle
         val board = arrayOf(
             intArrayOf(5, 3, 0, 0, 7, 0, 0, 0, 0),
             intArrayOf(6, 0, 0, 1, 9, 5, 0, 0, 0),
@@ -27,6 +28,58 @@ class SudokuTest {
         println("Puzzle solved: $solved")
         println("Iterations used: ${sudoku.iterations}")
         
+        // Check that it solved s
         assertTrue(solved, "The puzzle should be solvable")
+    }
+
+    @Test
+    fun testAnotherPuzzle() {
+        println("Testing another sudoku puzzle...")
+        
+        val board = arrayOf(
+            intArrayOf(0, 0, 0, 6, 0, 0, 4, 0, 0),
+            intArrayOf(7, 0, 0, 0, 0, 3, 6, 0, 0),
+            intArrayOf(0, 0, 0, 0, 9, 1, 0, 8, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 5, 0, 1, 8, 0, 0, 0, 3),
+            intArrayOf(0, 0, 0, 3, 0, 6, 0, 4, 5),
+            intArrayOf(0, 4, 0, 2, 0, 0, 0, 6, 0),
+            intArrayOf(9, 0, 3, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 2, 0, 0, 0, 0, 1, 0, 0)
+        )
+
+        val sudoku = Sudoku(board)
+        val solved = sudoku.solve()
+
+        println("Puzzle solved: $solved")
+        println("Iterations used: ${sudoku.iterations}")
+        
+        assertTrue(solved, "This puzzle should also be solvable")
+    }
+
+    @Test
+    fun testUnsolvablePuzzle() {
+        println("Testing unsolvable puzzle (invalid board)...")
+        
+        // Invalid puzzle
+        val board = arrayOf(
+            intArrayOf(5, 5, 0, 0, 7, 0, 0, 0, 0),
+            intArrayOf(6, 0, 0, 1, 9, 5, 0, 0, 0),
+            intArrayOf(0, 9, 8, 0, 0, 0, 0, 6, 0),
+            intArrayOf(8, 0, 0, 0, 6, 0, 0, 0, 3),
+            intArrayOf(4, 0, 0, 8, 0, 3, 0, 0, 1),
+            intArrayOf(7, 0, 0, 0, 2, 0, 0, 0, 6),
+            intArrayOf(0, 6, 0, 0, 0, 0, 2, 8, 0),
+            intArrayOf(0, 0, 0, 4, 1, 9, 0, 0, 5),
+            intArrayOf(0, 0, 0, 0, 8, 0, 0, 7, 9)
+        )
+
+        val sudoku = Sudoku(board)
+        val solved = sudoku.solve()
+
+        println("Puzzle solved: $solved")
+        println("Iterations used: ${sudoku.iterations}")
+        
+        assertFalse(solved, "Invalid puzzle should NOT be solvable")
     }
 }
